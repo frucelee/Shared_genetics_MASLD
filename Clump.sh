79084219625fbd12d77a9f23c87315a6c7c99687
Shifang Li; fruceslee@gmail.com <77853035+frucelee@users.noreply.github.com>
2026-05-27 09:07:26 +0200

    Clump.sh

--- Clump.sh
+++ Clump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-awk '$2<=5e-08' ${id} >output1.txt
+awk '$2<=5e-08' CPASSOC_GCST90627749_GCST90728570.tsv >output1.txt
 sed  -i '1i SNP P' output1.txt
 plink --bfile /scratch/users/s/h/shifang/ldsc/MAGMA/g1000_eur --clump output1.txt  --clump-r2 0.2 --out T123 --clump-kb 1000 --clump-p1 5e-8 --clump-p2 1e-5 --threads 8
-awk '{print $1,$3}' T123.clumped > ${id}_SNP.txt
-sed '1d' ${id} |awk '{print $2}' | head -n -2 > output.txt
+awk '{print $1,$3}' T123.clumped > CPASSOC_GCST90627749_GCST90728570_clup_SNP.txt
+sed '1d' CPASSOC_GCST90627749_GCST90728570_clup_SNP.txt |awk '{print $2}' | head -n -2 > CPASSOC_GCST90627749_GCST90728570_SNP_only.txt
