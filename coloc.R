4e5caf1f8ca42f2dea6fa7cee9727bacce5a985c
Shifang Li; fruceslee@gmail.com <77853035+frucelee@users.noreply.github.com>
2026-06-01 10:49:46 +0200

    coloc.R

--- coloc.R
+++ coloc.R
@@ -11,8 +11,8 @@ N3 <- as.numeric(args[5])
 N4 <- as.numeric(args[6])
 colnames(data1)=c("SNP","hg18","bq","A2","A1", "beta","pval", "se", "eaf", "n", "z" )
 colnames(data2)=c("SNP","hg18","bq","A2","A1", "beta","pval", "se", "eaf", "n", "z" )
-data1$varbeta <- (data1$SE)^2
-data2$varbeta <- (data2$SE)^2
+#data1$varbeta <- (data1$SE)^2
+#data2$varbeta <- (data2$SE)^2
 input <- merge(data1, data2, by="SNP", all=FALSE, suffixes=c("_eqtl","_gwas"))
 library("coloc")
 result <- coloc.abf(dataset1=list(pvalues=input$pval_eqtl, snp=input$SNP, type="cc", s=N1, N=N2,MAF=input$eaf_eqtl), dataset2=list(pvalues=input$pval_gwas,MAF=input$eaf_gwas, snp=input$SNP, type="quant", N=N4))
