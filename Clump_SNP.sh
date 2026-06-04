75412c7c46c5a320e291f1acc6fce9727b438f9c
Shifang Li; fruceslee@gmail.com <77853035+frucelee@users.noreply.github.com>
2026-06-04 13:46:29 +0200

    Clump_SNP.sh

--- Clump_SNP.sh
+++ Clump_SNP.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-awk '$2<=5e-08' CPASSOC_gwas_GCST90627749_GCST90728570.tsv >output1.txt
-sed  -i '1i SNP P' output1.txt
-plink --bfile /scratch/users/s/h/shifang/ldsc/MAGMA/g1000_eur --clump output1.txt  --clump-r2 0.2 --out T123 --clump-kb 1000 --clump-p1 5e-8 --clump-p2 1e-5 --threads 8
-awk '{print $1,$3}' T123.clumped > CPASSOC_GCST90627749_GCST90728570_clup_SNP.txt
+awk '$2<=5e-08' CPASSOC_gwas_GCST90627749_GCST90728570.tsv >output.txt
+sed  -i '1i SNP P' output.txt
+plink --bfile /scratch/users/s/h/shifang/ldsc/MAGMA/g1000_eur --clump output.txt  --clump-r2 0.2 --out tmp --clump-kb 1000 --clump-p1 5e-8 --clump-p2 1e-5 --threads 8
+awk '{print $1,$3}' tmp.clumped > CPASSOC_GCST90627749_GCST90728570_clup_SNP.txt
 sed '1d' CPASSOC_GCST90627749_GCST90728570_clup_SNP.txt |awk '{print $2}' | head -n -2 > CPASSOC_GCST90627749_GCST90728570_SNP_only.txt
+rm -r output.txt tmp* 
